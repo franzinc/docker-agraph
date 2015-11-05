a93b97cfd9d3389083223511808dfe91e5d49fcc
Kevin Layer <layer@franz.com>
2015-11-04 16:15:31 -0800

    run.sh: update for Docker 1.9.0

--- run.sh
+++ run.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
+# Tested on Docker 1.9.0.
 
 if [ "$(uname)" != "Darwin" ]; then
-    echo $0: script only runs on Mac OS X.
+    echo $0: this script only runs on Mac OS X.
     exit 1
 fi
 
 vbm="/Applications/VirtualBox.app/Contents/MacOS/VBoxManage"
-vm="boot2docker-vm"
+# The default name given by Docker 1.9.0:
+vm="default"
 
 set -eux
 
 docker run -d -p 10000-10035:10000-10035 --name agraph franzinc/agraph
 
-sudo "$vbm" controlvm $vm natpf1 delete "tcp-port10035"
-sudo "$vbm" controlvm $vm natpf1 "tcp-port10035,tcp,,10035,,10035"
+echo The IP of the container just started: $(docker-machine ip $vm)
+
+if ! "$vbm" controlvm $vm natpf1 delete "tcp-port10035"; then
+    echo delete of NAT rule failed, but maybe it did not exist
+fi
+"$vbm" controlvm $vm natpf1 "tcp-port10035,tcp,,10035,,10035"
