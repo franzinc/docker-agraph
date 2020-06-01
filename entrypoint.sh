franzinc/docker-agraph

commit dfb58946028b184bad4acdea2f02f87fd901fe3e

Replace recursive chown with find/chown to avoid CoW expansion
Recursive chown at the beginning of the entrypoint is necessary to
ensure whole /agraph directory is owned by agraph:agraph for AG to
operate correctly, but it causes copy-on-write expansion and slows
down the container startup. The new approach only applies chown to
files in the /agraph/data and /agraph/etc directories (mountpoints
of the data and config volumes, respectively) that are not already
owned by agraph:agraph.

Change-Id: Id3542970950a66c3a86fec312b60a5377fc08666

entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-# Recursively change ownership of the agraph directory (included
-# volumes /agraph/data and /agraph/etc).
-sudo chown -R agraph:agraph /agraph
-
+AG_RUNAS_USER=agraph
+AG_DATA_DIR=/agraph/data
+AG_CFG_FILE=/agraph/etc/agraph.cfg
+AG_LOG_FILE=/agraph/data/agraph.log
 
 # Make sure shared memory requirements are met.
 shm_size=$(df -P /dev/shm | grep -v Filesystem | awk '{print $2}')
@@ -17,6 +17,13 @@ option in order to operate correctly:
 EOF
     exit 1
 fi
+
+# Find all files not owned by $AG_RUNAS_USER in the /agraph/data and
+# /agraph/etc directories (volume mount points) and change ownership
+# to $AG_RUNAS_USER:$AG_RUNAS_GROUP.
+find /agraph/data /agraph/etc \! -user agraph -exec sudo chown $AG_RUNAS_USER {} +
+# Allow $AG_RUNAS_USER access to /agraph/lib/patches directory.
+sudo chown $AG_RUNAS_USER /agraph/lib/patches
 
 function file_env {
     local var="$1"
@@ -35,12 +42,8 @@ function file_env {
     export "$var"="$val"
 }
 
-AGDATADIR=/agraph/data
-AGRAPHCFG=/agraph/etc/agraph.cfg
-AGRAPHLOG=/agraph/data/agraph.log
-
-# Configure agraph if $agraphcfg file does not exist.
-if [ ! -f $AGRAPHCFG ]
+# Configure agraph if $AG_CFG_FILE file does not exist.
+if [ ! -f $AG_CFG_FILE ]
 then
     file_env 'AGRAPH_SUPER_USER'
     file_env 'AGRAPH_SUPER_PASSWORD'
@@ -59,13 +62,13 @@ EOF
     fi
     /agraph/lib/configure-agraph                  \
         --non-interactive                         \
-        --config-file    $AGRAPHCFG               \
-        --data-dir       $AGDATADIR               \
-        --log-dir        $AGDATADIR               \
+        --config-file    $AG_CFG_FILE             \
+        --data-dir       $AG_DATA_DIR             \
+        --log-dir        $AG_DATA_DIR             \
         --super-user     "$AGRAPH_SUPER_USER"     \
         --super-password "$AGRAPH_SUPER_PASSWORD" \
         --session-ports  10000-10034              \
-        --runas-user     agraph
+        --runas-user     $AG_RUNAS_USER
     # Install license from a variable or from a file, if supplied.
     file_env 'AGRAPH_LICENSE'
     if [ -n "$AGRAPH_LICENSE" ]; then
@@ -75,7 +78,7 @@ fi
 
 function terminate {
     echo Shutting down AllegroGraph
-    /agraph/bin/agraph-control --config $AGRAPHCFG stop
+    /agraph/bin/agraph-control --config $AG_CFG_FILE stop
     exit 0
 }
 
@@ -89,11 +92,11 @@ then
     exec "$@"
 else
     # Start AllegroGraph daemon.
-    /agraph/bin/agraph-control --config $AGRAPHCFG start
+    /agraph/bin/agraph-control --config $AG_CFG_FILE start
     # Monitor the logfile.
     # This pattern (& to put the process in the background and
     # then blocking using 'wait') appears to be the most reliable
     # way of getting bash to respond to signals.
-    tail -f $AGRAPHLOG &
+    tail -f $AG_LOG_FILE &
     wait $!
 fi

Dockerfile
@@ -19,37 +19,34 @@ RUN if [ -f "${AG_ARCHIVE##*/}" ];                                        \
 
 
 
-# Stage 1 - copy the AG installed during stage 0 into a clean Ubuntu
-# base and proceed building the rest of the image.
+# Stage 1 - prepare a clean Ubuntu, install dependencies, setup a user
+# and copy the AG installed during stage 0.
 FROM ubuntu:bionic
 MAINTAINER Franz Support <support@franz.com>
-
-COPY --from=installation-stage /agraph /agraph
 
 # Install the same dependencies as for the stage0 (installation) and
 # remove apt registries.
 RUN apt-get update && apt-get install -y openssl openssl1.0 sudo \
         && rm -rf /var/lib/apt/lists/*
 
-# Create agraph user, enable passwordless sudo and manually copy skel
-# files to the agraph user's home (useradd -m cannot be used, since
-# the /agraph directory already exists). For .sudo_as_admin_successful
-# file details see https://askubuntu.com/questions/22607.
-RUN groupadd -r agraph                                                  \
-        && useradd -M -g agraph -G sudo -s /bin/bash -d /agraph agraph  \
-        && echo "agraph ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers        \
-        && touch /agraph/.sudo_as_admin_successful                      \
-        && cp -r /etc/skel/. /agraph
+# Create agraph user, enable passwordless sudo and silence the "To run
+# a command as administrator ..." notification (for more details, see
+# https://askubuntu.com/questions/22607).
+RUN useradd -m -G sudo -s /bin/bash -d /agraph agraph            \
+        && echo "agraph ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers \
+        && touch /agraph/.sudo_as_admin_successful
+
+COPY --from=installation-stage /agraph /agraph
 
 ENV PATH=/agraph/bin:$PATH
+
+USER agraph
+WORKDIR /agraph
 
 VOLUME /agraph/data
 VOLUME /agraph/etc
 
 EXPOSE 10000-10034 10035
-
-USER agraph
-WORKDIR /agraph
 
 COPY entrypoint.sh /
 ENTRYPOINT ["/entrypoint.sh"]

commit faabb2990de9c820ba3234270a1ec0b93be20c14

Restore license injection behavior
The regression was introduced in dfb5894, when a few variables
were renamed, but one reference to $AGRAPHCFG was left behind,
breaking the license injection behavior.

Change-Id: Ie688ac2076dc0df33bba427e3e8992310a4af815

entrypoint.sh
@@ -72,7 +72,7 @@ EOF
     # Install license from a variable or from a file, if supplied.
     file_env 'AGRAPH_LICENSE'
     if [ -n "$AGRAPH_LICENSE" ]; then
-        echo "$AGRAPH_LICENSE" >> $AGRAPHCFG
+        echo "$AGRAPH_LICENSE" >> $AG_CFG_FILE
     fi
 fi
 